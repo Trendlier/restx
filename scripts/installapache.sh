929497ce46a4ac41ba7f14f328fb8d3e7677b904
Aswani <aswani.k@mistminds.com>
2015-05-05 13:57:31 +0530

    script for start server

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "restx install apache"
+yum -y install httpd > /var/log/installapache.out 2>&1
--- scripts/restartapache.sh
+++ scripts/restartapache.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "restx restart"
+service httpd restart > /var/log/restartapache.out 2>&1
