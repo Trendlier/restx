0b6a4f5d8c91220da4aba81a688b69ec26cfa7ca
sitarammist <sita.r@mistminds.com>
2015-05-06 18:40:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-cd /var/www/restx
-
 java -cp "/var/www/restx/target/restx/classes:/var/www/restx/target/dependency/*" -Drestx.app.package=rxinvoice -Drestx.mode=prod $VM_OPTIONS  rxinvoice.AppServer
